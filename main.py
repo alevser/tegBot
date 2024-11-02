ec863b988ad20d85c09998343b323234c75a7f5b
alevser <alevser@gmail.com>
2024-11-02 13:55:43 +0300

    adding new command

--- main.py
+++ main.py
@@ -18,13 +18,29 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Привет! Я - Каори, будем знакомы! Меня создали для того, чтобы сделать мир лучше. Отправь сообщения и я подскажу, какой твой ID. ")
 
-@dp.message()
+
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Проверочка!")
+
+
+
+
+"""@dp.message()
 async def message_handler(msg: types.Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
+    await msg.answer(f"Твой ID: {msg.from_user.id}")"""
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    dp.message.register(cmd_test2, Command("test2"))
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
