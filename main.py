7e200ee83e1fd419379d5e809c405db226eccb64
alevser <alevser@gmail.com>
2024-11-13 13:44:07 +0300

    cross chat sending + list

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config import BOT_TOKEN
-import time
+from aiogram.enums.dice_emoji import DiceEmoji
+from datetime import datetime
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -12,6 +13,24 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
+dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+
+
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
@@ -29,15 +48,26 @@ async def cmd_test1(message: types.Message):
 async def cmd_test2(message: types.Message):
     await message.reply("Проверочка!")
 
+@dp.message(Command("answer"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
 
 
+@dp.message(Command("reply"))
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message, bot: Bot):
+    await bot.send_dice(362441942, emoji=DiceEmoji.DICE)
 
-"""@dp.message()
+
+@dp.message(Command("id"))
 async def message_handler(msg: types.Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")"""
+    await msg.answer(f"Твой ID: {msg.from_user.id}")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    await dp.start_polling(bot, mylist=[1, 2, 3])
     dp.message.register(cmd_test2, Command("test2"))
     await dp.start_polling(bot)
 
